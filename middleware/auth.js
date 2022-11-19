958a7182017a293e1ee9c41f1068eb95b0398c34
dhrumilbhut <dhrumilbhut@gmail.com>
2022-11-19 17:02:32 +0530

    initial commit

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  console.log(req.cookies);
+  const { token } = req.cookies;
+
+  // if token is not there
+  if (!token) {
+    return res.status(403).send("Token is missing!");
+  }
+
+  // Verify token
+  try {
+    const decode = jwt.verify(token, "shhhhh");
+    console.log(decode);
+    req.user = decode;
+  } catch (error) {
+    res.status(403).send("Token is invalid!");
+  }
+  return next();
+};
+
+module.exports = auth;
